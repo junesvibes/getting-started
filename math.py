87c6b7b026e2718f53346ab27844ab9bbad94222
junesvibes <71022093+junesvibes@users.noreply.github.com>
2020-09-10 23:10:38 -0400

    Add files via upload

---
+++ math.py
@@ -0,0 +1,44 @@
+space="\n"
+print( 3 * (4 + 5 ))
+#Using numbers for math
+print (space)
+
+print(10%3)
+print(space)
+
+from math import *
+
+my_num = -4049166203
+print(str(my_num) + " my favorite number")
+print(space)
+#Using string function to combine phrases and words
+print(space)
+
+print(abs(my_num))
+#Using the absolute function to find the absolute number
+print(space)
+
+print(pow(3, 2))
+#Using the power function to put numbers against the power of another number
+print(space)
+
+print(max (3, 2))
+#Max function tells you which of the two numbers are bigger
+print(space)
+
+print(min (3, 2))
+#Min function tells you which of the two numbers are smaller
+print(space)
+
+print(round(3.2))
+print(round(3.7))
+#The round function rounds up the decimal to nearest whole number.")
+print(space)
+
+print(floor(3.7))
+#Floor function looks for the lowest number essentially chopping off the decimal point
+print(space)
+
+print(sqrt(36))
+#The sqrt function returns the number back to the square root
+print(space)
---
+++ strings.py
@@ -0,0 +1,41 @@
+space= "\n"
+character_name = "Mike"
+character_age= "50"
+is_Male= False
+print("There once was a man named " + character_name + ",")
+print("he was " + character_age + " years old")
+
+character_name = "Tommy"
+print("He really liked the name " + character_name + ",")
+print("but didn't like being " + character_age + ".")
+
+phrase = "Giraffe Academy"
+
+
+print(phrase + " is cool!")
+#Using the addition sign to add things with strings
+print(space)
+
+print(phrase.upper() .isupper())
+#Making things uppercase and true and falsing it
+print(space)
+
+print(len(phrase))
+#Checking the length of a string
+print(space)
+
+print(phrase[3])
+#Using the square bracket to find a certain character with the corrensponding number phrase always start with zero
+print(space)
+
+print(phrase.index("G"))
+#Using the index to find the corrensponding number with the character
+print(space)
+
+print(phrase.index ("Academy"))
+#Using the index to find the corrensponding number with the word
+print ("\n")
+
+print(phrase.replace("Giraffe", "Elephant"))
+#Using the replace function to replace words
+print (space)
